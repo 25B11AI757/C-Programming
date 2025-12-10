2b9f1137bf0d6d5ccd39da2290b2553d38680555
25B11AI757 <kavyamorla1223@gmail.com>
2025-12-10 18:58:27 +0530

    Add files via upload

---
+++ square root.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main( )
+{
+	int x,y;
+	printf("enter any number");
+	scanf("%d",&x);
+	y=sqrt(x);
+	printf("square root of a given number in=%d",y);
+	return 0;
+	
+}
