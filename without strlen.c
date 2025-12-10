27b63db9b2eb34ac316807ecbecea2af0f72443b
25B11AI757 <kavyamorla1223@gmail.com>
2025-12-10 18:54:37 +0530

    Add files via upload

---
+++ without strlen.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main( )
+{
+	char s[20];
+	int i,l;
+	printf("Enter string name");
+	scanf("%s",&s);
+	for(i=0;s[i]!=0;i++);
+	l=i;
+	printf("length of the string=%d",l);
+	return 0;
+}
