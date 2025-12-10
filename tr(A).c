a73a6d8322090af2ddf6623fc4d2fcae2a4f99eb
25B11AI757 <kavyamorla1223@gmail.com>
2025-12-10 18:50:20 +0530

    Add files via upload

---
+++ tr(A).c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main( )
+{
+	int A[10][10],r,c,i,j;
+	printf("Enter A matrix rows and columns size");
+	scanf("%d%d",&r,&c);
+	printf("Enter A matrix values");
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			scanf("%d",&A[i][j]);
+		}
+	}
+	printf("Transpose matrix is:\n");
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			printf("%d",A[j][i]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
